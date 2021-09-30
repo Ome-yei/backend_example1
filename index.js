534c6aedbe8b72da165b31c4faf954af79d27bab
Carlos Placencia <carlosplacencia2012@gmail.com>
2021-09-30 08:29:45 -0500

    display frontend

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cors())
 
 // Allows express to show static content
-// app.use(express.static('build'))
+app.use(express.static('build'))
 
 // You can use Morgan to log evry request made to server.
 
